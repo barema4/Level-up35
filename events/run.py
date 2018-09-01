e918e2c449282cd6af5993b24eec39afd12fdb62
barema4 <samrubarema6@gmail.com>
2018-09-01 14:09:39 -0700

    edited css for media queries [#160169209]

--- events/run.py
+++ events/run.py
@@ -4,11 +4,9 @@ def registration_checker():
     vistor = input("Enter is your first name: ")
     category = input("Enter your category:")
 
-    
     if vistor and (category == "vip"):
         return reception.check_vip(vistor)
     elif vistor and (category == "ordinary"):
         return reception.check_ord(vistor)
    
-
 print(registration_checker())
