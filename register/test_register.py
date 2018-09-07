0542d25f767b2d6982438e184e1c3c301ad1482f
barema4 <samrubarema6@gmail.com>
2018-09-07 11:15:46 -0700

    Add more tests

---
+++ register/test_register.py
@@ -0,0 +1,49 @@
+import unittest
+
+from register import Registration
+
+class RegistrationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = Registration()
+    def test_signup_creation(self):
+
+        self.assertIsInstance(self.app, Registration)
+    
+    def test_length(self):
+        
+        self.assertEqual(len(self.app.data), 0)
+
+        self.app.add("rubarema", "sam")
+        self.assertEqual(len(self.app.data), 1)
+
+
+    def test_return_lastname(self):
+
+        self.app.add("rubarema", "sam")
+        self.assertEqual(self.app.get_lastname("rubarema"),'sam')
+
+    def test_missing_key(self):
+        
+        with self.assertRaises(KeyError):
+           self.app.get_lastname("sum")
+    
+    def test_username(self):
+        self.f = self.app.add_credentials("barema4", "12355")
+        self.assertEqual(self.f, ('barema4', '12355'))
+    
+
+    def test_contact(self):
+        self.address = self.app.add_address("samrubarema6@gmail.com", "+256786666349")
+        self.assertEqual(self.address, ('samrubarema6@gmail.com', '+256786666349'))
+
+    def test_type(self):
+        f = self.app.add_credentials("barema4", "12355")
+        self.assertTrue(type(f)== tuple)
+
+    def test_member(self):
+        f = self.app.add_credentials("barema4", "12355")
+        self.assertIn('barema4',f)
+
+
+
+   
